b46230d60a9c6c3acf033289528d15cfa4b0f74c
natchin76 <57409149+natchin76@users.noreply.github.com>
2019-11-06 01:13:56 +0530

    Add files via upload

---
+++ fibo_function.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 22:27:40 2019
+Fibonacci sequence function file
+@author: CHINTAN
+"""
+def fibo(n):
+    if n==1:
+        return([1])
+    elif n==2:
+        return([1,1])
+    else:
+        s=[1,1]
+        for i in range(2,n):
+            s.append(s[i-1]+s[i-2])
+        return(s)
+ 
